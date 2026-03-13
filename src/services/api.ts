2a0a5b9e6c88c0f6503b62f147e13e0c7c6fcf7b
Adittyaaa-codes <aditya.mallick.1512@gmail.com>
2026-03-14 03:11:51 +0530

    feat: initialize Axios API client with base URL, credentials, and JWT authentication interceptor.

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,3 +4,11 @@ export const api = axios.create({
   baseURL: 'https://backend-xboi.onrender.com/api',          
   withCredentials: true,    
 })
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
