af3032facd9a366851665256a49dda93f7d4f6d6
Adittyaaa-codes <aditya.mallick.1512@gmail.com>
2026-03-23 05:18:46 +0530

    minor change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: 'https://backend-xboi.onrender.com/api',          
   withCredentials: true,    
 })
 
